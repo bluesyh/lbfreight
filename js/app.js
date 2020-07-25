59cb31c7b387bca9377794f8fc25b51b9b7e629a
bluesy <hblues@hotmail.com>
2020-07-25 14:01:00 +0100

    baseline

---
+++ js/app.js
@@ -0,0 +1,8 @@
+﻿var context = {
+    "name": "Holly Blues",
+    "occupation": "developer"
+}
+
+var templateScript = Handlebars.templates.header(context);
+
+$(document.body).append(templateScript);
